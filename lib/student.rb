e063645ecd9c3aa93d6ec442cc5db97f8f4f56a5
Sharon Watkins <swatkins876@gmail.com>
2019-11-12 10:19:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,8 @@ def update
     id = row[0]
     name = row[1]
     grade = row[2]
-    self.new(id, name, grade)
+    new_student = self.new(name, grade, id)
+    new_student
   end
 
   def self.find_by_name(name)
