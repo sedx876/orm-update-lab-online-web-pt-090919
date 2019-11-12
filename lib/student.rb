4191baf5654e9e162ed4f687c0b527ae572c62bc
Sharon Watkins <swatkins876@gmail.com>
2019-11-12 10:03:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,4 +11,15 @@ def initialize(name, grade, id=nil)
   @id = id
 end
 
+def self.create_table
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+  )
+  SQL
+  DB[:conn].execute(sql)
+end
+
 end
